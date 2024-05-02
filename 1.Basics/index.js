14ba54e4d96a369fb1208edc09e791b10ae9d5da
konda ravilla <kondaravilla000@gmail.com>
2024-05-02 09:16:28 +0530

    first commit

---
+++ 1.Basics/index.js
@@ -0,0 +1,44 @@
+
+
+
+var a = 'sachin'
+
+console.log('value:', a ,', datatype:', typeof a)
+
+var b = 40
+console.log('value:', b ,', datatype:', typeof b)
+
+var c = true
+console.log('value:', c ,', datatype:', typeof c)
+
+var d
+console.log('value:', d ,', datatype:', typeof d)
+
+var e = null
+
+console.log('value:', e, ', datatype:', typeof e)
+
+var f = {
+  name: 'kohli'
+}
+console.log('value:', f, ', datatype:', typeof f)
+
+var g = [1, 2, 3]
+
+console.log('value:', g, ', datatype:', typeof g)
+console.log(Array.isArray(g))
+
+var h = () => {
+  
+  var x = 100
+}
+console.log('value:', h, ', datatype:', typeof h)
+
+h()
+
+console.log(a, typeof a)
+
+
+
+
+
