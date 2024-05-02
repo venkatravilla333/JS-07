3dd925f9d75084c9732272ada9e1ae706d8c96e8
konda ravilla <kondaravilla000@gmail.com>
2024-05-02 10:16:20 +0530

    operators

---
+++ 2.Operators/index.js
@@ -0,0 +1,119 @@
+
+
+
+
+//1 Arithmatic operators
+
+// var x = 20
+
+// var y = 10
+
+// console.log(x + y) //add
+
+// console.log(x - y) //substraction
+
+// console.log(x * y) //multification
+
+// console.log(x / y) //division
+
+// console.log(x % y) //remainder of divison
+
+// console.log(x ** y) //exponential
+
+// console.log(++x) //pre increment
+// console.log(x++) //post increment
+// console.log(x)
+
+// console.log(--x) //pre decrement
+// console.log(x--) //post decrement
+// console.log(x)
+
+//2 Assignment operator
+
+// var x = 10
+
+// console.log(x += 5)
+// console.log(x -= 5)
+// console.log(x *= 5)
+// console.log(x /= 5)
+// console.log(x %= 5)
+// console.log(x **= 5)
+
+
+//3 Comparision operators
+
+// var x = 10
+
+// var y = 11
+
+// var y = 10
+
+// var y = '10'
+
+// var x = undefined
+// var y = null
+
+// var x = {value:10}
+// var y = {value:10}
+
+// var y = x
+
+// var x = [1,2,3]
+// var y = [4, 5, 6]
+
+// var x = [1,2,3]
+// var y = [1,2,3]
+
+// var x = [1, 2, 3]
+// var y = x
+
+//Relational operators
+
+// console.log( x > y)
+// console.log( x >= y)
+// console.log( x < y)
+// console.log(x <= y)
+
+//Equality operators
+
+// console.log( x == y) //loose equality (it compares only value)
+// console.log(x === y) //strict equality (it compares value and its data type)
+
+// InEquality
+
+
+
+// console.log( x != y) //loose inequality (it compares only value)
+// console.log(x !== y) //strict inequality (it compares value and its data type)
+
+//4 Ternary operator
+
+// var age = 15
+// var age = 20
+
+// var vote = age >= 18 ? 'Having vote right' : 'Not having vote right'
+
+// console.log(vote)
+
+//5 Logical operators
+
+// var income = true
+// var cibil = false
+
+// var hdfcCreditCard = income && cibil //Logical AND
+
+// console.log(hdfcCreditCard)
+
+// var rblCreditCard = income || cibil //Logical OR
+
+// console.log(rblCreditCard)
+
+
+// console.log(!income) // not operator
+
+
+
+
+
+
+
