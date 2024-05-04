6665b54262b53d7edf92ae0f3f04c7a7f8be1539
konda ravilla <kondaravilla000@gmail.com>
2024-05-04 10:39:05 +0530

    loops

---
+++ 4.Loops/index.js
@@ -0,0 +1,142 @@
+
+
+//Conditional Loops
+
+// for loop
+
+var a = 10
+
+
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+
+
+// for (var i = 1; i <= 100; i++){
+//   console.log(i, a)
+// }
+
+
+// while loop
+
+// var i = 1
+
+// while (i <=100) {
+//   console.log(i, a)
+//   i++
+// }
+
+
+// do while
+
+// var i = 101
+
+// do {
+//   console.log(i, a)
+//   i++
+// }while(i<=100)
+
+
+// infinity loop
+
+// for (var i = 1; i > 0; i++){
+//   console.log(i, a)
+// }
