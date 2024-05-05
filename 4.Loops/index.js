200cc33a8b083a42d1bddeb3a8f76a9f6d2ff97f
konda ravilla <kondaravilla000@gmail.com>
2024-05-05 11:17:29 +0530

    for in & for of loop

--- 4.Loops/index.js
+++ 4.Loops/index.js
@@ -140,3 +140,129 @@ var a = 10
 // for (var i = 1; i > 0; i++){
 //   console.log(i, a)
 // }
+
+// break
+
+// for (var i = 1; i <= 100; i++){
+//   if (i === 50) {
+//     break
+//   }
+//   console.log(i)
+// }
+
+
+// var i = 1
+
+// while (i <= 100) {
+//   if (i === 50) {
+//     break
+//   }
+//   console.log(i)
+//   i++
+// }
+
+// var i = 1
+
+// do {
+//   if (i === 50) {
+//     break
+//   }
+//   console.log(i)
+//   i++
+// }while(i<=100)
+
+// for (var i = 1; i > 0; i++){
+//   if (i === 50000) {
+//     break
+//   }
+//   console.log(i)
+// }
+
+
+// continue
+
+// for (var i = 1; i <= 100; i++){
+//   if (i === 50) {
+//     continue
+//   }
+//   console.log(i)
+// }
+
+// var i = 1
+
+// while (i <= 100) {
+//   if (i === 50) {
+//     i++
+//     continue
+//   }
+//   console.log(i)
+//   i++
+// }
+
+// var i =1
+
+// do {
+//    if (i === 50) {
+//     i++
+//     continue
+//   }
+//   console.log(i)
+//   i++
+  
+// }while(i<=100)
+
+// for (var i = 1; i > 0; i++){
+//   if (i === 10) {
+//     continue
+//   }
+//   if (i === 50) {
+//     break
+//   }
+//   console.log(i)
+// }
+
+
+// for in loop
+
+var person = {
+  name: 'kohli',
+  age: 30,
+  height: 5,
+  color: 'red'
+}
+console.log(person)
+
+
+// without using loop
+
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.height)
+// console.log(person.color)
+
+// with using loop
+
+// for (var x of person) {
+//   console.log(x, person[x])
+// }
+
+
+
+
+var arr = [1, 2, 3, 4, 5]
+console.log(arr)
+// without using loop
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+
+// for (var x of arr) {
+//   console.log(x)
+// }
+
+// for (var x in arr) {
+//   console.log(x, arr[x])
+// }
