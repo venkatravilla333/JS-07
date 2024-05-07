b45790e91ada3aef9a5d4af03b891bb5242e9385
konda ravilla <kondaravilla000@gmail.com>
2024-05-07 10:23:11 +0530

    default params

--- 5.Functions/index.js
+++ 5.Functions/index.js
@@ -20,3 +20,28 @@
 // }
 
 // hello()
+
+//Params vs arguments
+
+// var  test = (a,b,c)=> {
+//   console.log(a)
+//   console.log(b)
+//   console.log(c)
+// }
+// test(1,2,3)
+
+
+// Defalut parameter (ES-6)
+
+
+// function test(a=1,b=2,c=3, d=4, e=undefined, f=null) {
+//   console.log(a)
+//   console.log(b)
+//   console.log(c)
+//   console.log(d)
+//   console.log(e)
+//   console.log(f)
+// }
+// test(10, true, undefined, null, null, undefined)
+
+
