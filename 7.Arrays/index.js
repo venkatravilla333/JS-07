6bfa7fba056e2ec24f850736d224101b3c0e1c83
konda ravilla <kondaravilla000@gmail.com>
2024-05-23 10:40:22 +0530

    arrays

--- 7.Arrays/index.js
+++ 7.Arrays/index.js
@@ -67,16 +67,16 @@
 
 //How to remove all items from array
 
-var arr1 = [1, 2, 3, 4, 5]
+// var arr1 = [1, 2, 3, 4, 5]
 
-var arr2 = arr1
+// var arr2 = arr1
 
-console.log(arr1)
-console.log(arr2)
+// console.log(arr1)
+// console.log(arr2)
 
 //length
 
-console.log(arr1.length)
+// console.log(arr1.length)
 
 // arr1.length = 0
 
@@ -85,8 +85,264 @@ console.log(arr1.length)
 // arr1 = []
 
 
-console.log(arr1)
-console.log(arr2)
+// console.log(arr1)
+// console.log(arr2)
+
+
+
+//How to find items in array
+
+//primitives
+
+// var arr = [1, 2, 3, 4, 5, 2]
+
+// console.log(arr.indexOf(2))
+// console.log(arr.indexOf(20))
+// console.log(arr.lastIndexOf(2))
+// console.log(arr.lastIndexOf(20))
+// console.log(arr.includes(2))
+// console.log(arr.includes(20))
+
+//reference data types
+
+var arr = [{ course: 'react' }, { course: 'nodejs' }]
+
+//these methods dont work for reference types
+
+// console.log(arr.indexOf({course: 'react'}))
+// console.log(arr.lastIndexOf({course: 'react'}))
+// console.log(arr.includes({course: 'react'}))
+
+// var findValue = arr.find((obj) => {
+//   return obj.course === 'nodejs'
+// })
+
+// console.log(findValue)
+
+// var findIndex = arr.findIndex((obj) => {
+//   return obj.course === 'nodejs'
+// })
+
+// console.log(findIndex)
+
+
+//How to iterate array
+
+var arr = [1, 2, 3, 4, 5]
+
+//without loop
+
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+
+// for of
+
+//It iterates only values
+
+// for (var value of arr) {
+//   console.log(value)
+// }
+
+//It iterates only keys [index num]
+
+// for (var key in arr) {
+//   console.log(key)
+// }
+
+//forEach
+
+// var value = arr.forEach((v, i) => {
+//   console.log(v, i)
+
+//   // return 100
+// })
+
+// console.log(value)
+
+// How to copy / clone
+
+// var originalArr = ['sachin', { road: 101 }, () => { }]
+
+
+// var copiedArr = originalArr  //Normal copy
+// var copiedArr =JSON.parse(JSON.stringify(originalArr))  //Deep copy
+// var copiedArr = Object.assign([], originalArr) //shallow copy
+// var copiedArr = originalArr.slice() //shallow copy
+// var copiedArr = [...originalArr] //shallow copy (ES-6)
+
+// console.log('original arr', originalArr)
+// console.log('copied arr', copiedArr)
+
+// copiedArr[0] = 'kohli'
+// copiedArr[1].road = 201
+
+// console.log('original arr', originalArr)
+// console.log('copied arr', copiedArr)
+
+//Merging of arrays into one array
+
+// var arr1 = [1, 2, 3, 4, 5]
+// var arr2 = [6, 7, 8, 9, 10]
+// var arr3 = [11, 12, 13, 14, 15]
+
+// console.log(arr1)
+// console.log(arr2)
+
+// ES-5
+
+// var mergedArr1 = arr1.concat(arr2, arr3)
+// var mergedArr2 = arr2.concat(arr1, arr3)
+// console.log(mergedArr1)
+// console.log(mergedArr2)
+
+//ES-6 (Spread operator)
+
+// var mergedArr1 = [...arr1, ...arr2, ...arr3]
+// var mergedArr2 = [...arr3, ...arr2, ...arr1]
+
+// console.log(mergedArr1)
+// console.log(mergedArr2)
+
+//Join method
+
+// var arr = [1, 2, 3, 4, 5]
+
+// console.log(arr, typeof arr)
+// console.log(Array.isArray(arr))
+
+// var joinResult = arr.join()
+
+// console.log(joinResult, typeof joinResult)
+
+// var spiltResult = joinResult.split()
+
+// console.log(spiltResult, typeof spiltResult)
+
+
+//Testing of array items
+
+// var arr = [1, 2, 3, 4, 5, 6]
+// var arr = [1, 2, 3, 4, 5, -6]
+// var arr = [-1, -2, -3, -4, -5, -6]
+
+// var someResult = arr.some((value) => {
+//   return value >= 0
+// })
+
+// console.log(someResult)
+
+// var everyResult = arr.every((value) => {
+//   return value >= 0
+// })
+
+// console.log(everyResult)
+
+//Filter method
+
+// var arr = [1, 2, 3, 4, 5]
+
+// var filteredArr = arr.filter((value) => {
+//   return value > 2
+// })
+
+// console.log(filteredArr)
+
+// map method
+
+// var arr = [1, 2, 3, 4, 5]
+
+// var mapValue = arr.map((value) => {
+//   return value * 100
+// })
+
+// console.log(arr)
+// console.log(mapValue)
+
+// map vs forEach
+
+// var foreachValue = arr.forEach((value) => {
+//   return value * 100
+// })
+
+// console.log(foreachValue)
+
+// var arr = [1, 2, 3, 4, 5]
+
+// var reduceResult = arr.reduce((cum, cur) => {
+//   console.log(cum, cur)
+//   return cum+cur
+// }, 0)
+
+// console.log(reduceResult)
+
+//Flattening of an array
+
+// var arr = [1, 2, [100, 200, [1000, 2000, ['sachin', 'kohli']]]]
+
+//flat
+
+// var singleArr = arr.flat(3)
+// var singleArr = arr.flat(Infinity)
+
+// console.log(singleArr)
+
+// var singleArr = []
+
+// function flattenFun(arr) {
+//   // console.log(arr)
+//   for (var value of arr) {
+//     console.log(value)
+//     if (Array.isArray(value)) {
+//       flattenFun(value)
+//     } else {
+//       singleArr.push(value)
+//     }
+//   }
+// }
+
+// flattenFun(arr)
+
+// console.log(singleArr)
+
+//How to remove duplicate items from array
+
+// var arr = [1,2,3,2,4,2,3,2,5]
+
+//Set with spread operator
+
+// var uniqueArr = [...new Set(arr)]
+
+// console.log(uniqueArr)
+
+// var uniqueArr = []
+
+// function removeDuplicate(arr) {
+
+//   for (var value of arr) {
+//     // console.log(value)
+//     if (uniqueArr.indexOf(value) === -1) {
+//       uniqueArr.push(value)
+//     }
+//   }
+  
+// }
+
+// console.log(arr)
+// removeDuplicate(arr)
+
+// console.log(uniqueArr)
+
+
+
+
+
+
+
+
 
 
 
