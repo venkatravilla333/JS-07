b41d6e97bb727edcc2f1e60df64f9c7a7f5b3058
konda ravilla <kondaravilla000@gmail.com>
2024-06-05 09:46:56 +0530

    post & put & delete

--- 9.Async-Js/Api-calls/api.js
+++ 9.Async-Js/Api-calls/api.js
@@ -1,3 +1,5 @@
+//get all items from db
+
 var getAlldata = document.getElementById('get-alldata')
 
 
@@ -8,8 +10,102 @@ function getAlldataFun() {
   }).then((data) => {
   console.log(data)
 })
+}
+
+getAlldata.addEventListener('click', getAlldataFun)
+
+//get single item from db
+
+var getSingledataBtn = document.getElementById('get-singledata')
+
+
+
+function getSingledataFun() {
+  fetch('https://jsonplaceholder.typicode.com/posts/2')
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      console.log(data)
+  })
+}
+
+getSingledataBtn.addEventListener('click', getSingledataFun)
+
+//how to send data to db
+
+var senddataBtn = document.getElementById('send-data');
+
+var newObj = {
+  userId: 110,
+  title: 'hello ap',
+  body: 'This is body'
+}
+
+function sendDataFun() {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newObj)
+  })
+    .then((res) => {
+    console.log(res)
+    })
+    .catch((err) => {
+    console.log(err)
+  })
+}
+
+senddataBtn.addEventListener('click', sendDataFun)
+
+//How to update data in db
+
+var updateBtn = document.getElementById('update-data')
+
+
+function updateFun() {
+  fetch('https://jsonplaceholder.typicode.com/posts/10', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newObj),
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  
+}
+
+updateBtn.addEventListener('click', updateFun)
+
+//How to delete db item
+
+var deleteBtn = document.getElementById('delete-data')
+
+function deleteFun() {
+
+   fetch('https://jsonplaceholder.typicode.com/posts/5', {
+     method: 'DELETE',
+   })
+     .then((res) => {
+       console.log(res);
+     })
+     .catch((err) => {
+       console.log(err);
+     });
   
   
 }
 
-getAlldata.addEventListener('click', getAlldataFun)
+deleteBtn.addEventListener('click', deleteFun)
+
+
+
+
+
