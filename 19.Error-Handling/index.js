e48761cf3eedb9657061468c23e9eef185b56697
konda ravilla <kondaravilla000@gmail.com>
2024-06-12 10:48:37 +0530

    Error-handling

---
+++ 19.Error-Handling/index.js
@@ -0,0 +1,40 @@
+
+
+// Types of errors
+
+// syntax error
+
+// var obj = {
+//   name = 'sachin'
+// }
+
+//reference error
+
+
+
+// console.log(x)
+
+// function test() {
+//   // console.log(x)
+//   x = 10
+//   console.log(x)
+//   }
+//   test()
+
+
+//Type error
+
+// var x = 100
+
+// x.push(20)
+// console.log(x)
+
+
+
+//Error handling
+
+// console.log(test())
+// function test() {
+//   console.log(100)
+// }
+// console.log('hello')
