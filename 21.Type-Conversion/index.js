706d0733a933de2e66c0a6db2f837901b77d35ab
konda ravilla <kondaravilla000@gmail.com>
2024-06-14 10:50:57 +0530

    type conversion

---
+++ 21.Type-Conversion/index.js
@@ -0,0 +1,104 @@
+
+
+//implicit conversion
+
+//numaric string with + operator
+
+// console.log('5' + 'sachin')
+// console.log('5' + 5)
+// console.log('5' + true)
+// console.log('5' + undefined)
+// console.log('5' + null)
+// console.log('5' + {})
+// console.log('5' + [])
+// console.log('5' + function(){})
+
+//numaric string with -*/% operators
+
+// console.log('5' - 'sachin')
+// console.log('5' - 5)
+// console.log('5' - true)
+// console.log('5' - undefined)
+// console.log('5' - null)
+// console.log('5' - {})
+// console.log('5' - [])
+// console.log('5' - function(){})
+
+//non numaric string with +-*/%
+
+// console.log('sachin' + 'hi')
+// console.log('sachin' - 'hi')
+// console.log('sachin' - 10)
+// console.log('sachin' - true)
+// console.log('sachin' - false)
+// console.log('sachin' - undefined)
+// console.log('sachin' - null)
+// console.log('sachin' - {})
+// console.log('sachin' - [])
+// console.log('sachin' - function(){})
+
+//undefined with + - * / %
+
+// console.log(undefined + 'hi')
+// console.log(undefined + 10)
+// console.log(undefined - 'hi')
+// console.log(undefined - 10)
+// console.log(undefined - true)
+// console.log(undefined - false)
+// console.log(undefined - undefined)
+// console.log(undefined - null)
+// console.log(undefined - {})
+// console.log(undefined - [])
+// console.log(undefined - function(){})
+
+//Boolean with + - * / %
+
+// console.log(true + 'hi')
+// console.log(true + 10)
+// console.log(true - 'hi')
+// console.log(true - 10)
+// console.log(true - true)
+// console.log(true - false)
+// console.log(true - undefined)
+// console.log(true - null)
+// console.log(true - {})
+// console.log(true - [])
+// console.log(true - function(){})
+
+//null with + - * / %
+
+// console.log(null + 'hi')
+// console.log(null + 10)
+// console.log(null - 'hi')
+// console.log(null - 10)
+// console.log(null - true)
+// console.log(null - false)
+// console.log(null - undefined)
+// console.log(null - null)
+// console.log(null - {})
+// console.log(null - [])
+// console.log(null - function(){})
+
+
+//explicit conversion
+
+// var num = 10
+
+// console.log(typeof num)
+
+// var str = String(num)
+// var bool = Boolean(num)
+// var obj = Object(num)
+
+// console.log(typeof str)
+// console.log(typeof bool)
+// console.log(typeof obj)
+
+
+// var x = true
+
+// console.log(typeof x)
+// var str = String(x)
+// console.log(typeof str)
+// var num = Number(x)
+// console.log(typeof num)
