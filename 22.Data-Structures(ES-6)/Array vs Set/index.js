f0177911da2cc23b88fe3dec78ea59172e4d2492
konda ravilla <kondaravilla000@gmail.com>
2024-06-17 10:25:11 +0530

    array vs set

---
+++ 22.Data-Structures(ES-6)/Array vs Set/index.js
@@ -0,0 +1,135 @@
+
+
+//how to create
+
+// var arr = []
+// console.log(arr)
+
+// var mySet = new Set()
+// console.log(mySet)
+
+//how to initilize
+
+// var arr = [1, 2, 3, 3, 2]
+// console.log(arr)
+
+// var mySet = new Set([1,2,3,3,2])
+// console.log(mySet)
+
+//how to add new items
+
+// var arr = []
+
+// arr.push(1)
+// arr.push(2)
+// arr.push(3)
+// arr.push(3)
+// console.log(arr)
+
+// var mySet = new Set()
+
+// mySet.add(1)
+// mySet.add(2)
+// mySet.add(3)
+// mySet.add(3)
+// console.log(mySet)
+
+//how to access values
+
+// var arr = [1, 2, 3]
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+
+// var mySet = new Set([1, 2, 3, 3])
+
+// var myArr = [...mySet]
+// console.log(mySet)
+// console.log(myArr)
+
+// console.log(myArr[0])
+// console.log(myArr[1])
+// console.log(myArr[2])
+
+//how to find specific value
+
+// var arr = [1, 2, 3]
+
+// console.log(arr.indexOf(2) > -1)
+// console.log(arr.includes(2))
+
+// var mySet = new Set([1, 2, 3])
+
+// console.log(mySet.has(2))
+
+//how to delete specific value
+
+// var arr = [1, 2, 3]
+
+// arr.splice(1, 1)
+// console.log(arr)
+
+// var mySet = new Set([1, 2, 3])
+
+// mySet.delete(2)
+// console.log(mySet)
+
+//how to find length of items
+
+// var arr = [1, 2, 3]
+
+// console.log(arr.length)
+
+// var mySet = new Set([1, 2, 3])
+
+// console.log(mySet.size)
+
+//how to remove all items
+
+// var arr = [1, 2, 3]
+
+// arr.length = 0
+// arr = []
+// arr.splice(0, arr.length)
+// console.log(arr)
+
+// var mySet = new Set([1, 2, 3])
+
+// mySet.clear()
+// console.log(mySet)
+
+//how to loop
+
+var arr = [1, 2, 3]
+
+// for (var value of arr) {
+//   console.log(value)
+// }
+// for (var index in arr) {
+//   console.log(arr[index])
+// }
+// arr.forEach((v, i) => {
+//   console.log(v, i)
+// })
+
+// for (var i = 0; i < arr.length; i++){
+//   console.log(arr[i])
+// }
+
+// var x = arr.map((v, i) => {
+//   // console.log(v, i)
+//   return v
+// })
+
+// console.log(x)
+
+// var mySet = new Set([1,2,3])
+// console.log(mySet)
+
+// for (var value of mySet) {
+//   console.log(value)
+// }
+
+
+
