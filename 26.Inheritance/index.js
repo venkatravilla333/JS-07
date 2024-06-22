a17d00540f93973e81f225d1d39900cff852b15f
konda ravilla <kondaravilla000@gmail.com>
2024-06-22 10:40:15 +0530

    configurations

---
+++ 26.Inheritance/index.js
@@ -0,0 +1,107 @@
+
+
+//How to find prototype
+
+// var obj = {}
+// console.log(obj)
+
+// console.log(Object.getPrototypeOf(obj))
+// console.log(obj.__proto__)
+// console.log(Object.prototype)
+
+// function Test() {
+//   this.play = function () {
+//     console.log('own play')
+//   }
+// }
+// var obj1 = new Test()
+// console.log(obj1)
+
+// console.log(Object.getPrototypeOf(obj1))
+// console.log(obj1.__proto__)
+// console.log(Test.prototype)
+
+// var arr = []
+
+// console.log(arr)
+// console.log(Object.getPrototypeOf(arr))
+// console.log(arr.__proto__)
+// console.log(Array.prototype)
+
+// var obj = {
+//   name: 'sachin'
+// }
+
+// Object.prototype.play = () => {
+//   console.log('play')
+// }
+
+// console.log(Object.prototype)
+
+// obj.toString = () => {
+//   console.log('hello')
+// }
+
+// console.log(Object.prototype)
+
+// console.log(obj)
+// console.log(obj.toString())
+
+// for (var key in obj) {
+//   console.log(key)
+// }
+
+// var objectBase = Object.getPrototypeOf(obj)
+// console.log(objectBase)
+
+// var propertyDescriptor = Object.getOwnPropertyDescriptor(objectBase, 'toString')
+// console.log(propertyDescriptor)
+
+// Object.defineProperty(obj, 'name', {
+//   configurable: false,
+//   enumerable: false,
+//   writable: false
+// })
+
+// delete obj.name
+// obj.name = 'kohli'
+
+// console.log(obj)
+
+// console.log(Object.keys(obj))
+
+// for (var key in obj) {
+//   console.log(key)
+// }
+
+// console.log(Object.getOwnPropertyDescriptor(obj, 'name'))
+
+
+var myObj = {
+  name: 'dhoni',
+  age: 35,
+  // play: () => {
+  //   console.log('play')
+  // }
+}
+
+
+// var objBase = Object.prototype.go = function () { }
+// console.log(objBase)
+
+// Object.prototype.play = () => {
+//   console.log('play')
+// }
+
+// var objbase = Object.getPrototypeOf(myObj)
+// console.log(Object.getOwnPropertyDescriptor(objbase, 'play'))
+
+
+
+
+// console.log(Object.getOwnPropertyDescriptor(myObj, 'name'))
+// console.log(Object.getOwnPropertyDescriptor(myObj, 'age'))
+// console.log(Object.getOwnPropertyDescriptor(myObj, 'play'))
+
+
+
